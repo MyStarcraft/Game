020b2284915bc063a1bcaa8121e6f1ff10117735
clogic <choiseungil29@gmail.com>
2016-10-16 02:05:48 +0900

    first init

---
+++ Assets/Scripts/SceneCreate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneCreate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.SetResolution (1920, 1080, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
