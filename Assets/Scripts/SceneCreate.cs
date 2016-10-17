06e4221257a48cb6e7e9b8c9b5e1dc017eeb096d
clogic <choiseungil29@gmail.com>
2016-10-18 01:38:53 +0900

    create team select scene

--- Assets/Scripts/SceneCreate.cs
+++ Assets/Scripts/SceneCreate.cs
@@ -6,6 +6,7 @@ public class SceneCreate : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Screen.SetResolution (1920, 1080, true);
+		Debug.Log ("SceneCreate Start");
 	}
 	
 	// Update is called once per frame
